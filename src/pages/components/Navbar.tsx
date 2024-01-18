43a21b59adca4111c87ce1e45bb8670c28dbe9a1
Pedro <pedroafonsoleite11@gmail.com>
2024-01-18 17:03:21 -0300

    navbar

---
+++ src/pages/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <nav className="border-2 h-14 fixed top-0 flex w-full items-center justify-between
+        bg-white bg-opacity-70 border-b border-gray-200 backdrop-blur-[20px]">
+            <div>
+                LOGO
+            </div>
+        </nav>
+    )
+}
--- src/pages/feedprincipal/boxreview.tsx
+++ src/pages/feedprincipal/boxreview.tsx
@@ -1,11 +1,17 @@
 import BoxReview from "../components/Boxreview"
+import Navbar from "../components/Navbar"
 
 export default function PageBoxReview() {
     return (
-        <div className={``}>
-            <BoxReview />
-            <BoxReview />
-            <BoxReview />
+        <div>
+            <div>
+                <Navbar />
+            </div>
+            <div className={``}>
+                <BoxReview />
+                <BoxReview />
+                <BoxReview />
+            </div>
         </div>
     )
 }
