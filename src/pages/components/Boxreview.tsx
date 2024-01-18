05ddc4eeddb041d74426d2607ead536005e268ed
Pedro <pedroafonsoleite11@gmail.com>
2024-01-18 17:24:37 -0300

    muitas mudanças

--- src/pages/components/Boxreview.tsx
+++ src/pages/components/Boxreview.tsx
@@ -1,15 +1,16 @@
 export default function BoxReview() {
     return (
-        <div>
+        <div className="grid place-content-center">
+            <hr className="pt-14 w-[32rem] mx-2"/>
             <div className={`
-                bg-purple-500 m-2 rounded w-[32rem] h-[21rem]
+                m-2 rounded w-[32rem] h-[21rem]
             `}>
                 <img className="object-cover w-[32rem] h-[21rem] rounded" src="https://tm.ibxk.com.br/2023/11/12/12084659815000.jpg?ims=1200x675" alt="" />
             </div>
             <div className="mx-2 my-2 text-xl w-[32rem] font-bold">
             Metal Slug: Uma Odisseia Explosiva Pelos Corredores da Nostalgia
             </div>
-            <div className="mx-2 my-2 text-sm w-[32rem]">
+            <div className="mx-2 my-2 pb-10 text-sm w-[32rem]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis maximus id lacus at pretium. Suspendisse porttitor volutpat turpis, eget sodales arcu mattis porta. Suspendisse potenti. Phasellus metus magna, mollis sed finibus ut, feugiat et lorem...
             </div>
         </div>
--- src/pages/feedprincipal/boxreview.tsx
+++ src/pages/feedprincipal/boxreview.tsx
@@ -7,6 +7,9 @@ export default function PageBoxReview() {
             <div>
                 <Navbar />
             </div>
+            <div className="font-bold text-4xl text-center pt-32 pb-6">
+                feed
+            </div>
             <div className={``}>
                 <BoxReview />
                 <BoxReview />
